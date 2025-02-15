607d0823823e59f1e3e6d1c00d748c0b60ca5755
som3canadian <somecanadian0@gmail.com>
2025-02-15 01:57:13 -0500

    fixed python lib

--- lib/cf_kv.py
+++ lib/cf_kv.py
@@ -1,13 +1,21 @@
 import requests
 import base64
 import json
+from datetime import datetime
 
+### CONFIGURATION ###
 this_kv_worker_url = "<your-worker-url>"
 this_kv_worker_secret = "<your-secret>"
 this_kv_worker_custom_header = "<your-custom-header>"
 MIDDLEWARE_USE_ZERO_TRUST = False
 MIDDLEWARE_SERVICE_AUTH_CLIENT_ID = "<your-service-auth-client-id>"
 MIDDLEWARE_SERVICE_AUTH_CLIENT_SECRET = "<your-service-auth-client-secret>"
+
+default_expiration_days = 0
+default_metadata = {
+    "timestamp": datetime.now().isoformat()
+}
+### CONFIGURATION ###
 
 # get base64 of the data
 def b64_encode_json(data: str) -> str:
@@ -34,20 +42,23 @@ def request_builder(endpoint, secret):
 
 def list_namespaces():
     endpoint = f"/namespaces"
-    return request_builder(endpoint, this_kv_worker_secret)
+    response = request_builder(endpoint, this_kv_worker_secret)
+    return response['data']['namespaces']
 
 def list_kv_keys(namespace):
     endpoint = f"/list?limit=500&namespace={namespace}"
-    return request_builder(endpoint, this_kv_worker_secret)
+    response = request_builder(endpoint, this_kv_worker_secret)
+    return response['data']['keys']
 
 def get_key(key, namespace):
     endpoint = f"/get?key={key}&namespace={namespace}"
-    return request_builder(endpoint, this_kv_worker_secret)
+    response = request_builder(endpoint, this_kv_worker_secret)
+    return response['data']
 
 def get_key_value(key, namespace):
     endpoint = f"/get?key={key}&namespace={namespace}"
     response = request_builder(endpoint, this_kv_worker_secret)
-    value = response['data']['value']
+    value = response['data']['value']['value']
     return value
 
 def get_key_metadata(key, namespace):
@@ -56,7 +67,7 @@ def get_key_metadata(key, namespace):
     metadata = complete_response['data']['value']['metadata']
     return metadata
 
-def set_key_value(key, value, metadata={}, namespace=None, expiration=None):
+def set_key_value(key, value, metadata=default_metadata, namespace=None, expiration=default_expiration_days):
     base64_metadata = b64_encode_json(metadata)
     endpoint = f"/set?key={key}&value={value}&expiration={expiration}&metadata={base64_metadata}&namespace={namespace}"
     return request_builder(endpoint, this_kv_worker_secret)
@@ -68,3 +79,17 @@ def delete_key(key, namespace):
 def delete_all_keys(namespace):
     endpoint = f"/delete_all?namespace={namespace}"
     return request_builder(endpoint, this_kv_worker_secret)
+
+# Check if at least one namespace exists
+def at_least_one_namespace_exists():
+    endpoint = f"/namespaces"
+    response = request_builder(endpoint, this_kv_worker_secret)
+    response_length = len(response['data']['namespaces'])
+    if response_length == 0:
+        return False
+    else:
+        return True
+
+if at_least_one_namespace_exists() is False:
+    print("No namespaces found")
+    exit(1)
